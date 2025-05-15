7a451f3dc220eccbc03636c020509b0d0756d769
lordishow <143359715+lordishow@users.noreply.github.com>
2025-05-15 20:53:58 +0300

    Update anticheatbypass.lua

--- anticheatbypass.lua
+++ anticheatbypass.lua
@@ -1,6 +1,10 @@
 -- ADONIS BYPASS WASNT MADE BY ME
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pixeluted/adoniscries/refs/heads/main/Source.lua'))()
 -- the actual bypass
+local bypass_already_loaded = getgenv().whispering_pines_bypass_loaded_pz
+if bypass_already_loaded then print("Bypass Already Loaded (WP_PZ)") return end
+getgenv().whispering_pines_bypass_loaded_pz = true
+
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local Character = LocalPlayer.Character
 local old;
